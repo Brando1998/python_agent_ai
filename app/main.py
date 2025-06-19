1ff06aed3442103301eb7323ff7ba5b65587f6ce
Brando1998 <brandodiazmont@gmail.com>
2025-06-19 00:17:47 -0500

    chore: create main file

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from app.database import init_db
 from app.routes import chat_routes, user_routes, health_routes
 from app.config import USE_OPENAI_MOCK
@@ -6,11 +7,16 @@ from app.config import USE_OPENAI_MOCK
 print(f"🔁 Usando {'mock' if USE_OPENAI_MOCK else 'cliente real'} de OpenAI")
 
 
-app = FastAPI(title="Chatbot de Evaluación de Riesgos")
-
-@app.on_event("startup")
-def startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Init app
     init_db()
+    yield
+
+app = FastAPI(
+    title="Chatbot de Evaluación de Riesgos",
+    lifespan=lifespan
+)
 
 # Routes
 app.include_router(user_routes.router)
