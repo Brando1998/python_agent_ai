c55a31e57e976f51b93d464fe11dd76e92b2a6aa
Brando1998 <brandodiazmont@gmail.com>
2025-06-18 22:13:32 -0500

    feat: añadidas las rutas a main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 from app.database import init_db
+from app.routes import chat_routes, user_routes, health_routes
 
 app = FastAPI(title="Chatbot de Evaluación de Riesgos")
 
 @app.on_event("startup")
 def startup():
     init_db()
+
+# Routes
+app.include_router(user_routes.router)
+app.include_router(chat_routes.router)
+app.include_router(health_routes.router)
